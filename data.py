75ecb5eac07283daf3970729dc29ba6c5080f63d
SOORYA77 <ksoorya065@gmail.com>
2026-02-23 11:04:52 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,14 @@
+tourist_spots = [
+{
+"name": "Marina Beach",
+"lat": 13.0500,
+"lng": 80.2824,
+"visit_time": 90
+},
+{
+"name": "Fort St George",
+"lat": 13.0827,
+"lng": 80.2870,
+"visit_time": 60
+}
+]
